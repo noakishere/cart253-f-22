101338dca5c7af7400af4b625e0d156b1d9142f5
kamyar <kamyarkarimi98@gmail.com>
2022-10-25 14:11:20 -0400

    drawCricle works

--- exercise03/js/script.js
+++ exercise03/js/script.js
@@ -8,19 +8,59 @@ author, and this description to match your project!
 
 "use strict";
 
+const circleSize = 75;
+
 /**
 Description of preload
 */
-function preload() {
-	createCanvas(500, 500);
-}
+function preload() {}
 
 /**
 Description of setup
 */
-function setup() {}
+function setup() {
+	createCanvas(500, 500);
+	background(1, 1, 1);
+	// drawTarget();
+}
 
 /**
 Description of draw()
 */
 function draw() {}
+
+function drawTarget() {
+	let color1 = {
+		r: random(255),
+		b: random(255),
+		g: random(255),
+	};
+	let color2 = {
+		r: random(255),
+		b: random(255),
+		g: random(255),
+	};
+
+	for (var i = 0; i < 4; i++) {
+		if (i % 2 == 0) fill(color1.r, color1.b, color1.g);
+		else fill(color2.r, color2.b, color2.g);
+
+		circle(mouseX, mouseY, circleSize - 25 * i);
+	}
+
+	// fill(color1.r, color1.b, color1.g);
+	// circle(250, 250, 100);
+
+	// fill(color2.r, color2.b, color2.g);
+	// circle(250, 250, 75);
+
+	// fill(color1.r, color1.b, color1.g);
+	// circle(250, 250, 50);
+
+	// fill(color2.r, color2.b, color2.g);
+	// circle(250, 250, 25);
+}
+
+function mouseClicked() {
+	drawTarget();
+}
