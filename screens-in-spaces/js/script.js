0bfb43503ce694cbe879ea06d439b60fdae3b1bc
kamyar <kamyarkarimi98@gmail.com>
2022-10-31 19:38:40 -0400

    text container and button added.

--- screens-in-spaces/js/script.js
+++ screens-in-spaces/js/script.js
@@ -9,28 +9,8 @@ author, and this description to match your project!
 "use strict";
 
 /**
-Description of preload
-*/
-function preload() {}
-
-/**
-Description of setup
-*/
-function setup() {
-	createCanvas(1080, 720);
-	background("black");
-	ruler();
-
-	// drawWindows();
-	// drawWindows(30);
-
-	for (var i = 0; i < cols; i++) {
-		drawWindows(i * windowHeightGaps);
-	}
-
-	typeWriter("this is a test text to see how it'd look.", 2, 300, 550, 50);
-}
-
+ * Windows settings and how they should be drawn
+ */
 const rows = 17;
 const cols = 10;
 let windowGaps = 10;
@@ -46,9 +26,56 @@ let y = 0;
 let shouldDraw = true;
 
 /**
+ * Sounds
+ */
+let sounds = ["assets/sounds/bark.wav", "assets/sounds/pickupCoin.wav"];
+let p5songList = [];
+
+/**
+ * DOM elements
+ */
+let button;
+let pTextContainer;
+
+/**
+ * texts to be shown
+ */
+let texts = ["new text that is kinda long", "newer text that is even longer hehehe"];
+let textArrays = [];
+let textCounter = 0;
+
+/**
+Description of preload
+*/
+function preload() {
+	for (var i = 0; i < sounds.length; i++) {
+		p5songList[i] = loadSound(sounds[i]);
+	}
+}
+
+/**
+Description of setup
+*/
+function setup() {
+	createCanvas(1080, 720);
+	background("black");
+	ruler();
+
+	pTextContainer = createP("").addClass("text-container");
+	pTextContainer.position(500, 450, "fixed");
+
+	for (var i = 0; i < cols; i++) {
+		drawWindows(i * windowHeightGaps);
+	}
+	createMyButton();
+}
+
+/**
 Description of draw()
 */
-function draw() {}
+function draw() {
+	drawEyes();
+}
 
 function drawWindows(col = 0) {
 	x = 325;
@@ -61,20 +88,50 @@ function drawWindows(col = 0) {
 	}
 }
 
-function writeText(textToWrite) {
-	text("this is a test text to see how it'd look.", 300, 500);
-}
-
 function typeWriter(sentence, n, x, y, speed) {
+	button.style("display", "none");
+
 	if (n < sentence.length) {
 		text(sentence.substring(0, n + 1), x, y);
 		n++;
-		setTimeout(function () {
+		window.setTimeout(function () {
 			typeWriter(sentence, n, x, y, speed);
 		}, speed);
+	} else {
+		button.style("display", "block");
+		textCounter++;
+		print(`${textCounter} counter`);
 	}
 }
 
+function updateText(sentence) {
+	button.style("display", "none");
+
+	pTextContainer.html(sentence);
+
+	p5songList[textCounter].play();
+
+	if (textCounter <= textArrays.length) {
+		window.setTimeout(() => {
+			button.style("display", "block");
+			textCounter++;
+			print(`${textCounter} counter`);
+		}, 300);
+	}
+}
+
+function createMyButton() {
+	button = createButton("continue").addClass("myButton");
+	button.position(500, 650);
+	button.mousePressed(() => {
+		updateText(texts[textCounter]);
+	});
+}
+
+/*
+ * have a clearer idea of the points on the canvas
+ * only used for production, won't be used for the final result
+ */
 function ruler() {
 	for (let i = 1; i < 1080; i++) {
 		/* to have a clean look, as without this constraint
